58a1e8a323a16ecf10c5b0f58f04c7040f33067c
Kara Artem <kara@qtim.ru>
2022-09-30 01:39:49 +0300

    fix 1

--- src/media/entities/media.entity.ts
+++ src/media/entities/media.entity.ts
@@ -1,6 +1,5 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '../../common/entities/base.entity';
-import { UserEntity } from '../../users/entities/user.entity';
 
 @Entity('media')
 export class MediaEntity extends BaseEntity {
@@ -16,13 +15,13 @@ export class MediaEntity extends BaseEntity {
   @Column({ type: 'varchar' })
   mediaUrl: string;
 
-  @Column({ type: 'uuid', nullable: true })
-  ownerId?: string | null;
-
-  @ManyToOne(() => UserEntity, {
-    onDelete: 'SET NULL',
-    onUpdate: 'CASCADE',
-  })
-  @JoinColumn()
-  owner?: UserEntity | null;
+  // @Column({ type: 'uuid', nullable: true })
+  // ownerId?: string | null;
+  //
+  // @ManyToOne(() => UserEntity, {
+  //   onDelete: 'SET NULL',
+  //   onUpdate: 'CASCADE',
+  // })
+  // @JoinColumn()
+  // owner?: UserEntity | null;
 }
